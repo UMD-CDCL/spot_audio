3f4a72f639a0afde9df35c108b66e4974e759a48
Srijal97 <srijal97@gmail.com>
2026-02-17 21:42:03 -0500

    ensure confident in motor alertness classification before publishing

--- spot_audio/audio_classification_node.py
+++ spot_audio/audio_classification_node.py
@@ -254,8 +254,10 @@ class AudioClassificationNode(Node):
                         observation_module = ObservationModule.AST_ALERTNESS_VERBAL,
                         observation=verbal_alertness_classification.tolist()
                     )
-                    self.pub_observation.publish(alertness_verbal_observation)
-                    published_observation = True
+                    self.get_logger().warn(f'Alertness Verbal Detected: {alertness_verbal_observation.observation[0]}')
+                    if alertness_verbal_observation.observation[0] >= 0.60:
+                        self.pub_observation.publish(alertness_verbal_observation)
+                        published_observation = True
                 if respiratory_distress_label == 1 and  not torch.isnan(respiratory_distress_classification).any().item():
                     respiratory_distress_observation = Observation(
                         stamp=self.get_clock().now().to_msg(),
